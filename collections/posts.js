99c648d020b7ce62ef53878202056471125aae7b
Raghavendra Ugare <raghu.ugare@gmail.com>
2015-02-28 14:11:59 +0530

    Adding code to populate the posts collection if empty.

--- collections/posts.js
+++ collections/posts.js
@@ -1,1 +1,23 @@
 Posts = new Meteor.Collection('posts');
+
+if (Posts.find().count() === 0) {
+
+    Posts.insert({
+        title: 'Introducing Telescope',
+        author: 'Sacha Greif',
+        url: 'http://sachagreif.com/introducing-telescope/'
+    });
+
+    Posts.insert({
+        title: 'Meteor',
+        author: 'Tom Coleman',
+        url: 'http://meteor.com'
+    });
+
+    Posts.insert({
+        title: 'The Meteor Book',
+        author: 'Tom Coleman',
+        url: 'http://themeteorbook.com'
+    });
+
+}
