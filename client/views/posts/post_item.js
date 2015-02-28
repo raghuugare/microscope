7007bb4d0910a99fab853d6e97d864434966901f
Raghavendra Ugare <raghu.ugare@gmail.com>
2015-02-28 12:55:13 +0530

    Adding some extra info from <a> elements...

--- client/views/posts/post_item.js
+++ client/views/posts/post_item.js
@@ -2,7 +2,7 @@ Template.postItem.helpers({
     domain: function() {
         var a = document.createElement('a');
         a.href = this.url;
-        return a.protocol + a.hostname;
+        return a.protocol + '//' + a.hostname;
         // Note!
         // <a> element in JS implements the URLUtilsReadOnly interface that provides several read-only properties like protocol, href, hostname, etc.
         
